f51f45f6d42c49c734b9c9b9798e6600c397e16c
unknown <armon.naddaf@gmail.com>
2023-10-30 15:26:56 -0700

    new server runner script

---
+++ RunDualSPMSetup_noXY.ps1
@@ -0,0 +1,20 @@
+if (-not (Test-Path 'repos')) {
+    Write-Output "repos directory missing -- please run install script"
+    exit
+}
+
+function RunInNewTab($command) {
+    Start-Process -FilePath 'powershell.exe' -ArgumentList "-NoExit","-Command & {$command}"
+}
+
+RunInNewTab {
+    . .\repos\spm-venv\Scripts\Activate.ps1
+    Set-Location -Path 'repos/solvent_dispenser_ui'
+    python main.py 5004
+}
+
+RunInNewTab {
+    . .\repos\spm-venv\Scripts\Activate.ps1
+    Set-Location -Path 'repos/solvent_dispenser_ui'
+    python main.py 5003
+}
